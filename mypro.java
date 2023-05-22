89c4db5ccea887d70c0d40871bb732f7f6fdd69b
kishore500027 <112535762+kishore20057@users.noreply.github.com>
2023-05-22 10:25:24 +0530

    Add files via upload

---
+++ mypro.java
@@ -0,0 +1,22 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class mypro {
+
+	static testunit sc = new testunit();
+	
+	@Test
+	public static void test1() {
+
+        assertEquals(3, sc.Addition(1,2));
+        System.out.println(sc.Addition(1,2));
+    }
+	
+	@Test
+	public static void test2() {
+        assertEquals(2, sc.Subtraction(3,1));
+        System.out.println(sc.Subtraction(3,1));
+    }
+
+}
---
+++ testunit.java
@@ -0,0 +1,10 @@
+public class testunit{
+
+ 	int Addition(int a,int b) {
+        return a + b;
+    }
+	
+	int Subtraction(int a,int b) {
+        return a-b;
+    }
+}
